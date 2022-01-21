f175d20d162214410e99d3cd5c2073b0e9f04b13
Thilak002 <96661999+Thilak002@users.noreply.github.com>
2022-01-21 20:12:19 +0530

    Update PrimeNumber.java

--- day2/PrimeNumber.java
+++ day2/PrimeNumber.java
@@ -3,8 +3,8 @@ package week1.day2;
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		int num = 13;
-		boolean prime = false;
+		int num = 7;
+		boolean prime = true;
 		for (int i = 2; i<num; i++) {
 		
 			if(num % i == 0) {
